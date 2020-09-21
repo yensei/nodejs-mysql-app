7197ad26cd4d5e9718bd460eaf851af19c03ba23
juliom <julio.c.munhoz.p@gmail.com>
2020-09-21 16:34:13 -0400

    Implementamos el login

--- src/lib/passport.js
+++ src/lib/passport.js
@@ -30,6 +30,29 @@ passport.use('local.signup', new strategy({
   
 }));
 
+passport.use('local.signin', new strategy({
+   usernameField:'username',
+   passwordField:'password',
+   passReqToCallback:true
+}, async(req,username,password,done)=>{
+   try{
+      const rows = await pool.query('SELECT * FROM users WHERE username = ?',[username]);
+      if(rows.length>0){
+         const user = rows[0];
+         const validPassword = await helpers.mathPassword(password, user.password);
+         if(validPassword){
+            done(null, user, req.flash('success','Welcome '+user.fullname));
+         }else{
+            done(null, false,req.flash('message','Usuario o contraseña invalidad'));
+         }
+      }else{
+         return done(null,false,req.flash('message','Usuario o contraseña invalidad'));
+      }
+   }catch(e){
+      console.log(e);
+   }
+}));
+
 passport.serializeUser((user, done) => {
    done(null, user.id);
 });
--- src/routes/authentication.js
+++ src/routes/authentication.js
@@ -24,6 +24,18 @@ router.post('/signup', passport.authenticate('local.signup', {
 }
 ));
 
+router.get('/signin',(req,res)=>{
+    res.render('auth/signin');
+});
+
+router.post('/signin', (req,res,next)=>{
+    passport.authenticate('local.signin',{
+        successRedirect:'/profile',
+        failureRedirect:'/signin',
+        failureFlash:true
+    })(req,res,next);
+});
+
 
 router.get('/profile', (req, res) => {
     res.send('authenticate success');
