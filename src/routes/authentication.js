5e8c28829809c23e4453549ed283dfb86d6a12d6
juliom <julio.c.munhoz.p@gmail.com>
2020-09-21 17:22:20 -0400

    validacion de links por usuario

--- src/routes/authentication.js
+++ src/routes/authentication.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const passport = require('passport');
+const {isLoggedIn} = require('../lib/auth');
 
 router.get('/signup', (req, res) => {
     res.render('auth/signup');
@@ -24,7 +25,7 @@ router.post('/signup', passport.authenticate('local.signup', {
 }
 ));
 
-router.get('/signin',(req,res)=>{
+router.get('/signin', (req,res)=>{
     res.render('auth/signin');
 });
 
@@ -37,9 +38,16 @@ router.post('/signin', (req,res,next)=>{
 });
 
 
-router.get('/profile', (req, res) => {
-    res.send('authenticate success');
+router.get('/profile',isLoggedIn, (req, res) => {
+    res.render('profile');
 });
+
+
+router.get('/logout', (req, res) => {
+    req.logOut();
+    res.redirect('/signin');
+});
+
 
 
 module.exports = router;
--- src/routes/links.js
+++ src/routes/links.js
@@ -1,19 +1,21 @@
 const express = require("express");
 const router = express.Router();
+const {isLoggedIn} = require('../lib/auth');
 
 //DB Connection
 const pool = require("../database");
 
-router.get("/add", (req, res) => {
+router.get("/add",isLoggedIn, (req, res) => {
   res.render("links/add");
 });
 
-router.post("/add", async (req, res) => {
+router.post("/add",isLoggedIn, async (req, res) => {
   const { title, url, description } = req.body;
   const newLink = {
     title,
     url,
     description,
+    user_id: req.user.id
   };
 
   await pool.query("INSERT INTO links SET ?", [newLink]);
@@ -21,26 +23,26 @@ router.post("/add", async (req, res) => {
   res.redirect("/links");
 });
 
-router.get("/", async (req, res) => {
-  const links = await pool.query("SELECT * FROM links");
+router.get("/",isLoggedIn, async (req, res) => {
+  const links = await pool.query("SELECT * FROM links WHERE user_id = ?",[req.user.id]);
   req.flash('success','Successfully!');
   res.render("links/list", { links });
 });
 
-router.get("/delete/:id", async (req, res) => {
+router.get("/delete/:id",isLoggedIn,async (req, res) => {
   const { id } = req.params;
   await pool.query("DELETE FROM links WHERE id=?", [id]);
   req.flash('success','Successfully!');
   res.redirect("/links");
 });
 
-router.get("/edit/:id", async (req, res) => {
+router.get("/edit/:id",isLoggedIn, async (req, res) => {
   const { id } = req.params;
   const links = await pool.query("SELECT * FROM links WHERE id=?", [id]);
   res.render("links/edit", { link: links[0] });
 });
 
-router.post("/edit/:id", async (req, res) => {
+router.post("/edit/:id", isLoggedIn,async (req, res) => {
   const { id } = req.params;
 
   const { title, description, url } = req.body;
